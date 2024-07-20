1f5a1c6ce9c59a74adec3b3c630f60492c678439
rbrenor2 <rbrenorios@gmail.com>
2024-07-20 13:04:04 +0100

    Initial Commit

---
+++ BabyNameGen/BabyNameGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyNameGenApp.swift
+//  BabyNameGen
+//
+//  Created by Breno R R on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BabyNameGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyNameGenTests/BabyNameGenTests.swift
@@ -0,0 +1,17 @@
+//
+//  BabyNameGenTests.swift
+//  BabyNameGenTests
+//
+//  Created by Breno R R on 20/07/2024.
+//
+
+import Testing
+@testable import BabyNameGen
+
+struct BabyNameGenTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
